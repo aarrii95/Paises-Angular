db78519057d291a5fa7994fdb84decb1dfef1d9f
Ariel Subiabre <arielsubiabre95@gmail.com>
2021-12-23 16:54:13 -0300

    primer commit

---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
+})
+export class PorRegionComponent {
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  regionActiva: string = '';
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {}
+
+  getClasesCss(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string) {
+    //evitar refresh si la region activa es la misma
+    if(region === this.regionActiva) {return ;}
+
+    this.regionActiva = region;
+    this.paises = []
+
+    this.paisService.buscarRegion(region)
+      .subscribe(paises => (this.paises = paises));
+
+  }
+}
